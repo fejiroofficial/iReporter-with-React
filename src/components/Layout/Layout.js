cdd95102efc59948f0871d1048003c620ccb7b35
fejiroofficial <gpsparks@rocketmail.com>
2019-02-19 00:01:59 +0100

    Feat: Footer Component This is a commit for working on the footer component which should lie at the bottom of each web page.
    [Delivers #164029798]

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,15 +4,16 @@ import Aux from '../../hoc/Aux';
 import Classes from './Layout.css';
 import Header from '../Header/Header';
 import Toolbar from '../ToolBar/ToolBar';
+import Footer from '../Footer/Footer';
 const layout = (props) => (
   <Aux>
-    <Header/>
-    <Toolbar/>
+    <Header />
+    <Toolbar />
     <div>Toolbar, Sidedrawer, Backdrop</div>
     <main className={Classes.Content}>
       {props.children}
     </main>
-    <div>Footer</div>
+    <Footer />
   </Aux>
 );
 
