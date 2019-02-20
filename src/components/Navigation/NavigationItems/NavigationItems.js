8f6211c13b88a2b33973c75e872d64f7fa4d0b81
fejiroofficial <gpsparks@rocketmail.com>
2019-02-20 02:01:22 +0100

    chore -Navigation Items This is a commit for outsourcing navigation items to its component.
    [Fixes]

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Classes from './NavigationItems.css';
+
+const NavigationItems = (props) => (
+  <div className={Classes.LargeScreenNav}>
+    <div className={Classes.Seperator}></div>
+    <a href="./about_us.html">ABOUT US</a>
+    <div className={Classes.Seperator}></div>
+    <a href="./redFlag.html">RED-FLAGS</a>
+    <div className={Classes.Seperator}></div>
+    <a href="./interventions.html">INTERVENTIONS</a>
+    <div className={Classes.Seperator}></div>
+    <a href="./report.html">IREPORT</a>
+    <div className={Classes.Seperator}></div>
+    <a href="./learn_us.html">LEARN ABOUT CORRUPTION</a>
+    <div className={Classes.Seperator}></div>
+  </div>
+);
+
+export default NavigationItems;
--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -1,20 +1,9 @@
 import React from 'react';
+import Navigation from '../Navigation/NavigationItems/NavigationItems';
 import Classes from './ToolBar.css';
 
 const toolBar = (props) => (
-  <div className={Classes.LargeScreenNav}>
-    <div className={Classes.Seperator}></div>
-    <a href="./about_us.html">ABOUT US</a>
-    <div className={Classes.Seperator}></div>
-    <a href="./redFlag.html">RED-FLAGS</a>
-    <div className={Classes.Seperator}></div>
-    <a href="./interventions.html">INTERVENTIONS</a>
-    <div className={Classes.Seperator}></div>
-    <a href="./report.html">IREPORT</a>
-    <div className={Classes.Seperator}></div>
-    <a href="./learn_us.html">LEARN ABOUT CORRUPTION</a>
-    <div className={Classes.Seperator}></div>
-  </div>
+  <Navigation></Navigation>
 );
 
 export default toolBar;
