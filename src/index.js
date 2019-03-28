337541b242184393672b8abf9d6175e0d142bafa
Fejiro Gospel <38490848+fejiroofficial@users.noreply.github.com>
2019-03-28 10:26:10 +0100

    feat: Redux setup This is a commit for setting up a redux store. [ Delivers #164029759]

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(rootReducer);
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
