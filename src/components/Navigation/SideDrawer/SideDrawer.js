23482b5dfdbff1b6fb34b28a4f154c4482942e1a
Fejiro Gospel <38490848+fejiroofficial@users.noreply.github.com>
2019-05-02 15:02:31 +0100

    Improving records page design

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import Classes from './SideDrawer.css';
 
-const sideDrawer = (props) => (
-  <div className={Classes.Overlay}>
-    <button onClick={props.clicked} className={Classes.Closebtn}>&times;</button>
-    <div className={Classes.OverlayContent}>
-      <div className={Classes.Seperator} />
-      <NavLink to="/about" >ABOUT US</NavLink>
-      <div className={Classes.Seperator} />
-      <NavLink to="/corruption" >LEARN ABOUT CORRUPTION</NavLink>
-      <div className={Classes.Seperator} />
-      <NavLink to="/redflags" >REDFLAGS</NavLink>
-      <div className={Classes.Seperator} />
-      <NavLink to="/interventions" >INTERVENTIONS</NavLink>
-      <div className={Classes.Seperator} />
-      <NavLink to="/login" >LOGIN</NavLink>
-      <div className={Classes.Seperator} />
-      <NavLink to="/signup" >SIGN UP</NavLink>
-      <div className={Classes.Seperator} />
+const sideDrawer = (props) => {
+  const { isLoggedIn } = props;
+  return (
+    <div className={Classes.Overlay}>
+      <button onClick={props.clicked} className={Classes.Closebtn}>&times;</button>
+      <div className={Classes.OverlayContent}>
+        <div className={Classes.Seperator} />
+        <NavLink to="/about" >ABOUT US</NavLink>
+        <div className={Classes.Seperator} />
+        <NavLink to="/corruption" >LEARN ABOUT CORRUPTION</NavLink>
+        <div className={Classes.Seperator} />
+        {
+          isLoggedIn
+            ?
+            <>
+              <NavLink to="/user/ireport/red-flags">REDFLAGS</NavLink>
+              <div className={Classes.Seperator} />
+              <NavLink to="/user/ireport/interventions">INTERVENTIONS</NavLink>
+              <div className={Classes.Seperator} />
+              <NavLink to="/user/ireport" >IREPORT</NavLink>
+              <div className={Classes.Seperator} />
+            </>
+            :
+            <>
+              <NavLink to="/login" >LOGIN</NavLink>
+              <div className={Classes.Seperator} />
+              <NavLink to="/signup" >SIGN UP</NavLink>
+              <div className={Classes.Seperator} />
+            </>
+        }
+      </div >
     </div >
-  </div >
+  );
+};
+
+const mapStateToProps = state => (
+  {
+    isLoggedIn: state.authReducer.isLoggedIn,
+  }
 );
 
-export default sideDrawer;
+export default connect(mapStateToProps, {})(sideDrawer);
--- src/components/Records/Records.js
+++ src/components/Records/Records.js
@@ -27,19 +27,30 @@ const records = (props) => {
       <div className={Classes.RecordContainer}>
         {
           userRecords && userRecords.length !== 0 ?
-            userRecords.map((userRecord, index) => (
-              <RecordCard
-                key={index}
-                firstname={userRecord.firstname}
-                lastname={userRecord.lastname}
-                comment={userRecord.comment}
-                status={userRecord.status}
-                date={dateFormat(userRecord.createdon)}
-                image={userRecord.profile_image}
-                recordId={userRecord.id}
-                recordType={userRecord.type}
-              />
-            ))
+            userRecords.map((userRecord, index) => {
+              // let imageEvidence;
+              // console.log(userRecord.image_url);
+              // if (userRecord.image_url !== null) {
+              //   console.log('yoyoyoyo');
+              //   imageEvidence = userRecord.image_url;
+              // } else {
+              //   imageEvidence = 'https://images.huffingtonpost.com/2015-08-13-1439490178-5587538-HP90_No_Evidence_500x390-thumb.jpg';
+              // }
+              return (
+                <RecordCard
+                  key={index}
+                  firstname={userRecord.firstname}
+                  lastname={userRecord.lastname}
+                  comment={userRecord.comment}
+                  status={userRecord.status}
+                  date={dateFormat(userRecord.createdon)}
+                  recordImage={userRecord.image_url}
+                  image={userRecord.profile_image}
+                  recordId={userRecord.id}
+                  recordType={userRecord.type}
+                />
+              );
+            })
             : <div className={Classes.EmptyRecordContainer}>
             <RecordCard
               firstname=""
